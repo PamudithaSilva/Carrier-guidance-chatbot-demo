8770166bd715f7187bd53e951551813439fb98be
PamudithaSilva <shalukapamuditha@gmail.com>
2026-02-09 10:16:50 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Career Guidance Chatbot Backend is running 🚀")
+})
+
+// Chat API
+app.post("/chat", (req, res) => {
+  const userMessage = req.body.message
+
+  console.log("User message:", userMessage)
+
+  res.json({
+    reply: `You said: ${userMessage}`
+  })
+})
+
+
+// Start server
+const PORT = 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
