b4ad36b46db702750f20331661ab7f64fce6af79
PamudithaSilva <shalukapamuditha@gmail.com>
2026-02-09 10:26:14 +0530

    Adding env

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 
@@ -29,3 +30,5 @@ const PORT = 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
